126c125b8c4bf8b48fefba9dcdc0c6be6f835d0a
Dushyant <157586156+dushyantzz@users.noreply.github.com>
2024-12-02 12:25:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
+import os
 
 # Load the trained model
 with open("Symptom_detector.pkl", "rb") as model_file:
@@ -50,4 +51,5 @@ def predict():
 
 # Run the app
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
